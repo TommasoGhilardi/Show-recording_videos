0ae1d9c3e9f2e67e6335e2a259517e2734c4574e
ghila93 <38372956+ghila93@users.noreply.github.com>
2019-06-20 23:49:34 +0200

    s

--- study_windows.py
+++ study_windows.py
@@ -286,13 +286,13 @@ log.append('Session accepted'+' : '+str(time.time()))
 cap = cv2.VideoCapture(0 + cv2.CAP_DSHOW)
 print('Webcam activated')
 log.append('Webcam activated'+' : '+str(time.time()))
-web_width= cap.get(cv2.CAP_PROP_FRAME_WIDTH)
-web_height= cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
-height_w = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))*(160/int(web_width))
+width_w= cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+height_w= cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+print(str(width_w))
 
 time.sleep(0.5)
 fourcc = cv2.VideoWriter_fourcc(*"mp4v")#'DIVX', *'mp4v', *'X264',  [mp4 +'avc1'] [avi + 'DIVX']
-out = cv2.VideoWriter(final+'/data/webcam_'+num+'.mp4',fourcc,15,(160,height_w),isColor=False)
+out = cv2.VideoWriter(final+'/data/webcam_'+num+'.mp4',fourcc,15,(int(width_w)*0.3,int(height_w)*0.3),isColor=False)
 out.set(cv2.VIDEOWRITER_PROP_QUALITY,1)
 time.sleep(0.5)
 
@@ -314,7 +314,7 @@ def on_draw():
     window.clear()
     ret, frame = cap.read()
     if player.source and player.source.video_format:
-        frame=cv2.resize(cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY),(width_w,height_w),interpolation=cv2.INTER_NEAREST)
+        frame=cv2.resize(cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY),None,fx=0.3,fy=0.3,interpolation=cv2.INTER_NEAREST)
         player.get_texture().blit(pos1,pos2, width=dim1, height=dim2)
         out.write(frame)
         global fram
@@ -341,15 +341,6 @@ log.append('Video stop'+' : '+str(stop))
 fps = round(fram/(stop-start))
 log.append('Framerate of'+' : '+str(fps)+' fps')
 print(str(fps))
-
-# =============================================================================
-# saving logs
-# =============================================================================
-logg= open(final+'\\data\\log_'+num+'.txt','w+')
-for err in log:
-    logg.write(err+'\n') 
-logg.close() 
-time.sleep(1)
 
 # =============================================================================
 # BYBY
